667e40cf469de563d9762dc7545bf190caca1855
Artem Skichko <artem.skichko@student.tuke.sk>
2025-07-19 10:13:44 +0200

    adding new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,56 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterOutlet, ɵEmptyOutletComponent } from '@angular/router';
+import { TodoComponent } from "./components/todo/todo.component";
+import { Todo } from './types/todo';
+
+const todos = [
+  {id: 1, title: 'HTML + CSS', completed: true},
+  {id: 2, title: 'JS', completed: false},
+  {id: 3, title: 'React', completed: false},
+  {id: 4, title: 'Vue.js', completed: false},
+]
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, TodoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'todo-app';
+  todos = todos;
+  todoForm = new FormGroup({
+    title: new FormControl('', {  
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(3),
+      ],
+    }),
+  });
+  
+  get title() {
+    return this.todoForm.get('title') as FormControl;
+  };
+
+  get activeTodos() {
+    return todos.filter(todo => !todo.completed);
+  }
+
+  addTodo() {
+    if (this.todoForm.invalid) {
+      return;
+    }
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      title: this.title.value,
+      completed: false,
+    };
+
+    this.todos.push(newTodo);
+    this.todoForm.reset();
+  }
 }
---
+++ src/app/components/todo/todo.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Todo } from '../../types/todo';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.scss'
+})
+export class TodoComponent {
+  @Output() delete = new EventEmitter;
+
+  @Input() todo!: Todo;
+
+  editing = false;
+}
